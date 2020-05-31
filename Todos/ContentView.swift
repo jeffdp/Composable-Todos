975863f4ba24a60f87eea881fa10018342310b89
Jeff Porter <jeff.porter@acm.org>
2020-05-30 17:24:46 -0700

    Implement actions

--- Todos/ContentView.swift
+++ Todos/ContentView.swift
@@ -20,7 +20,8 @@ struct AppState: Equatable {
 }
 
 enum AppAction: Equatable {
-    
+    case todoCheckboxTapped(index: Int)
+    case todoTextFieldChanged(index: Int, text: String)
 }
 
 struct AppEnvironment {
@@ -29,9 +30,16 @@ struct AppEnvironment {
 
 let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
     switch action {
+    case .todoCheckboxTapped(index: let index):
+        state.todos[index].isComplete.toggle()
+        return .none
         
+    case .todoTextFieldChanged(index: let index, text: let text):
+        state.todos[index].description = text
+        return .none
     }
 }
+.debug()
 
 struct ContentView: View {
     let store: Store<AppState, AppAction>
@@ -40,8 +48,19 @@ struct ContentView: View {
         NavigationView {
             WithViewStore(self.store) { viewStore in
                 List {
-                    ForEach(viewStore.todos) { todo in
-                        Text(todo.description)
+                    ForEach(Array(viewStore.todos.enumerated()), id: \.element.id) { index, todo in
+                        HStack {
+                            Button(action: { viewStore.send(.todoCheckboxTapped(index: index)) }) {
+                                Image(systemName: todo.isComplete ? "checkmark.square" : "square")
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            TextField("Untitled todo", text: viewStore.binding(
+                                get: { $0.todos[index].description },
+                                send: { .todoTextFieldChanged(index: index, text: $0) }
+                            ))
+                        }
+                        .foregroundColor(todo.isComplete ? .gray : nil)
                     }
                 }
             .navigationBarTitle("Todos")
@@ -60,7 +79,7 @@ struct ContentView_Previews: PreviewProvider {
              isComplete: false),
         Todo(id: UUID(),
              description: "Beer",
-             isComplete: false),
+             isComplete: true),
     ]
     
     static var previews: some View {
